0206811f479db8d91584645d949fc802d30ccc13
BrettSantor <b.santor27@gmail.com>
2022-12-06 19:11:00 -0800

    fixed missing .js file

--- assets/dice.js
+++ assets/dice.js
@@ -1,21 +1,23 @@
 var dice = {
     sides: 20,
     roll: function () {
-        var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-        return randomNumber;
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
     }
-}
-
-//Prints dice roll to page
-
-function printNumber(number) {
-    var hexagon = document.getElementById('hexagon');
+  }
+  
+  
+  
+  //Prints dice roll to the page
+  
+  function printNumber(number) {
+    var placeholder = document.getElementById('hexagon');
     hexagon.innerHTML = number;
-}
-
-var button = document.getElementById('button');
-
-button.onclick = function() {
+  }
+  
+  var button = document.getElementById('button');
+  
+  button.onclick = function() {
     var result = dice.roll();
     printNumber(result);
-}
+  };
